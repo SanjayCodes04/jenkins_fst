a51c3d03d442586762453fa2a567757fcd869488
SanjayCodes27 <146862062+SanjayCodes27@users.noreply.github.com>
2023-12-25 09:55:41 +0530

    putting failed test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+
+  @Test
+  public void intentionallyFailed() {
+    assertTrue(false);
+  }
   
 
   @Test
